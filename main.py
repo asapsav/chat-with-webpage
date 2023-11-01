f0d005d925743c76764b4c7b1a3ccc9f957a42f3
Saveliy Kholin <holins12@gmail.com>
2023-10-31 17:24:16 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from playwright.sync_api import sync_playwright
+
+def describe_website(url = "https://google.com"):
+    p = sync_playwright().start()
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto(url)
+
+    # Click the first link on the page
+    #page.click("a")
+
+    # Get and print the current URL
+    new_url = page.url
+    print(f"New URL: {new_url}")
+
+while True:
+    pass
+
+# browser.close()  # Commented out to keep the browser open
+
